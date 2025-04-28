a4986d6578ca83fd398641ae45c45e7639798efd
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-04-28 17:10:32 +0100

    feat: add Recipes page and Recipe Gallery

--- src/components/Recipes/RecipeGallery.jsx
+++ src/components/Recipes/RecipeGallery.jsx
@@ -1,34 +1,47 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 
 import { RecipeFilter } from "./RecipeFilter";
 import { RecipeCard } from "../../shared";
 
 import { RecipeContext } from "../../contexts/RecipeContextProvider/RecipeContext";
 
+
 export const RecipeGallery = () => {
-    const { recipes, loading, error } = useContext(RecipeContext);
+    const { recipes, loading, error, handleNextPageSearch } = useContext(RecipeContext);
+
+
+    const rendersCount1 = useRef(0);
+    rendersCount1.current += 1;
+    // console.log("Recipe Gallery - " + rendersCount1.current);
+
+
+
 
     return (
-        <section>
+        <div className="w-[75%]">
             {error && <p>{error}</p>}
             {loading && <p>Loading...</p>}
-            {!error && !loading && (
-                <div className="container flex px-10 pt-[60px] pb-[90px] gap-10 justify-center">
-                    {/* <RecipeFilter /> */}
-                    {/* <div className="w-[75%] flex flex-wrap gap-10"> */}
+            {!error && !loading && recipes && (
+                <div className="flex flex-col gap-10 justify-center">
                     {recipes.length > 0 ? (
-                        <div className="w-[100%] flex flex-wrap gap-10">
-                            {recipes.map((recipe) => (
-                                <div className="w-[325px] h-[165px]">
-                                    <RecipeCard key={recipe.label} data={recipe} />
-                                </div>
-                            ))}
-                        </div>
+                        <>
+                            <div className="w-[100%] flex flex-wrap gap-10">
+                                {recipes.map((data) => (
+                                    <div
+                                        className="w-[325px] h-[165px]"
+                                        key={data.recipe.uri}
+                                    >
+                                        <RecipeCard data={data} />
+                                    </div>
+                                ))}
+                            </div>
+                            <button onClick={() => handleNextPageSearch()}>Next page</button>
+                        </>
                     ) : (
                         <p>Recipes not found</p>
                     )}
                 </div>
             )}
-        </section>
+        </div>
     );
 };
