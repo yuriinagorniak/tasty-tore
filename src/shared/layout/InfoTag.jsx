bd633252f6a25c587a364c34d436d7bfb4871b9b
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-04-28 17:15:22 +0100

    feat: add ShoppingList page

---
+++ src/shared/layout/InfoTag.jsx
@@ -0,0 +1,7 @@
+export const InfoTag = ({ label }) => {
+    return (
+        <div className="text-sm border p-1.5 rounded-2xl bg-[#e0cfa1] text-[#373737]">
+           {label}
+        </div>
+    );
+};
