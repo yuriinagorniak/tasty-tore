aa7a92d2f4d3b81d63d420b38bf662d17d7575bf
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-01 23:24:13 +0100

    fix: add dynamic label to RecipeInfoSection

--- src/components/RecipePage/RecipeInfoSection.jsx
+++ src/components/RecipePage/RecipeInfoSection.jsx
@@ -1,11 +1,11 @@
 import { InfoTag } from "../../shared";
 
-export const RecipeInfoSection = ({ recipe = {}, dataKey = null }) => {
+export const RecipeInfoSection = ({ recipe = {}, dataKey = null, label = "Info" }) => {
     return (
         <>
             {recipe && recipe[dataKey] && (
                 <section className="w-full flex flex-col justify-center items-center my-10 gap-4">
-                    <h3 className="text-2xl font-bold">Health</h3>
+                    <h3 className="text-2xl font-bold">{label}</h3>
                     <ul className="flex w-[70%] flex-wrap justify-center items-center gap-1">
                         {recipe[dataKey].map((title) => (
                             <li key={title}>
