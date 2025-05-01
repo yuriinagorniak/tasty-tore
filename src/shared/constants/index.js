c24cc67c47efd5f3a9a19b3f5b8ad86bb1114cdd
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-01 23:26:02 +0100

    feat: add constants (daysOfWeek and mealTypes)

---
+++ src/shared/constants/index.js
@@ -0,0 +1,10 @@
+export const daysOfWeek = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+];
+export const mealTypes = ["breakfast", "lunch", "dinner"];
