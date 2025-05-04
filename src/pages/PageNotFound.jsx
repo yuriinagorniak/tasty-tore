9b985996bc575e8c5b3185fa097f486f53100e40
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-04 15:29:59 +0100

    feat: add PageNotFound

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import { PageBanner } from "../shared"
+
+export const PageNotFound = () => {
+    return (
+        <div>
+            <PageBanner pageTitle="Oops... The page not found" />
+        </div>
+    )
+}
