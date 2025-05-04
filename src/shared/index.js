e0df6bdc8c5ef62aa14f82f6e56c2c2ebab0896a
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-04 13:44:28 +0100

    feat: add lazy load image to PageBanner

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,3 +1,2 @@
 export * from "./icons";
 export * from "./layout";
-export * from "./constants";
--- src/shared/layout/PageBanner.jsx
+++ src/shared/layout/PageBanner.jsx
@@ -1,13 +1,23 @@
-import DefaultBg from "../../assets/defaultBg.jpg";
+import DefaultBg from "../../assets/defaultBg-1.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
-export const PageBanner = ({ children = "", pageTitle = "Tasty tore", bg = DefaultBg }) => {
+export const PageBanner = ({ children = "", pageTitle = null, bg = DefaultBg }) => {
     return (
-        <section
-            className="tint-bg bg-cover bg-no-repeat bg-center"
-            style={{ backgroundImage: `url(${bg})` }}
-        >
-            <div className="container flex flex-col items-center pt-36 pb-40 text-center text-(--secondary-text-color) font-bold">
-                <h3 className="text-4xl">{pageTitle}</h3>
+        <section className="bg-cover bg-no-repeat bg-center relative">
+            <div className="absolute top-0 left-0 w-full h-full z-0 tint-bg overflow-hidden">
+                <LazyLoadImage
+                    alt={pageTitle}
+                    className="w-full h-full overflow-hidden object-cover"
+                    effect="blur"
+                    height="100%"
+                    src={bg} 
+                    width="100%"
+                />
+            </div>
+            <div className="relative z-3 container flex flex-col items-center pt-36 pb-40 text-center text-(--secondary-text-color) font-bold">
+                {pageTitle && <h3 className="text-4xl">{pageTitle}</h3>}
                 {children}
             </div>
         </section>
