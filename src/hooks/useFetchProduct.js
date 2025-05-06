8604eed9494863b5c613fe5e1a8d8ead4d619891
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-06 22:44:18 +0100

    fix: prevent multiple unnecessary API calls

--- src/hooks/useFetchProduct.js
+++ src/hooks/useFetchProduct.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const foodAppId = import.meta.env.VITE_FOOD_APP_ID;
 const foodAppKey = import.meta.env.VITE_FOOD_APP_KEY;
 
-export const useFetchProduct = (query = "") => {
+export const useFetchProduct = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     const [results, setResults] = useState(null);
 
-    const fetchProduct = async () => {
+    const fetchProduct = async (query = "") => {
         try {
             setLoading(true);
             const response = await axios.get("https://api.edamam.com/api/food-database/v2/parser", {
@@ -21,7 +21,6 @@ export const useFetchProduct = (query = "") => {
             });
 
             setResults(response.data);
-            
         } catch (err) {
             setError(err);
         } finally {
@@ -29,9 +28,6 @@ export const useFetchProduct = (query = "") => {
         }
     };
 
-    useEffect(() => {
-        fetchProduct();
-    }, [query]);
 
-    return { error, loading, results };
+    return { error, loading, results, fetchProduct };
 };
