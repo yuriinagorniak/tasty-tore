6a80b77da97bd582b4e7299810ce7132d1eb4097
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-19 03:50:49 +0100

    feat: add route constants in routes.js

---
+++ src/constants/routes.js
@@ -0,0 +1,13 @@
+export const ROUTES = {
+    AUTH: {
+        BASE: '/auth',
+        LOGIN: '/auth/login',
+        SIGNUP: '/auth/signup'
+    },
+    RECIPE_PAGE: '/recipe',
+    RECIPES: '/recipes',
+    DASHBOARD: '/dashboard',
+    FAVOURITES: '/favourites',
+    MEAL_PLANNER: '/meal-planner',
+    SHOPPING_LIST: '/shopping-list',
+}
