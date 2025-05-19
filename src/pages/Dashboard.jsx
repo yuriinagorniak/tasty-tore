3b2fab01032ed213d48bc8d5d56eb331c16cc1a5
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-19 03:59:42 +0100

    feat: update Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,19 @@
+import { PageBanner } from "../shared";
+import { useUser } from "../hooks/useUser";
+import { TransparentButton } from "../shared";
+
 export const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const { user, handleLogout } = useUser();
+    console.log("dashboard rerendered")
+    console.log(user.displayName || user.email);
+    return (
+        <>
+            <PageBanner pageTitle={`Welcome back, ${user.displayName || user.email}`} />
+            <div className="container flex items-center justify-center pt-10">
+                <div className="w-[200px]">
+                    <TransparentButton handleClick={handleLogout}>Log out</TransparentButton>
+                </div>
+            </div>
+        </>
+    );
 };
