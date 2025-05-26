db11022536085e0a765af061f8102998fc0b41f0
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-26 23:56:31 +0100

    chore: update authReducer

--- src/components/Auth/reducers/authReducer.js
+++ src/components/Auth/reducers/authReducer.js
@@ -1,4 +1,4 @@
-export const getInitialValue = (authOption) => ({
+export const getAuthInitialValue = (authOption) => ({
     isRegistering: authOption === "signup",
     displayName: "",
     email: "",
