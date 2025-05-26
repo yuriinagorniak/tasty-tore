6c2f33230e2e6bbd228a5e42020b9be82d29c394
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-05-26 23:54:53 +0100

    fix(HomePage): change project name in HeroSection

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -7,24 +7,10 @@ export const HeroSection = () => {
             <div className="flex items-center gap-10">
                 <MainLogo width={140} height={140} />
                 <div className="font-bold pt-4 flex flex-col gap-1">
-                    <h2 className="text-5xl">TASTY TROVE</h2>
+                    <h2 className="text-5xl">TASTY TORE</h2>
                     <p className="text-3xl">Taste the treasure!</p>
                 </div>
             </div>
         </PageBanner>
-        // <section
-        //     className="tint-bg bg-no-repeat bg-cover w-full h-[500px] flex flex-col items-center justify-center text-4xl"
-        //     style={{ backgroundImage: `url(${HeroSectionBg})` }}
-        // >
-        //     <div className="container flex flex-col items-center justify-center">
-        //         <div className="flex items-center gap-10">
-        //             <MainLogo width={140} height={140} />
-        //             <div className="font-bold pt-4 flex flex-col gap-1">
-        //                 <h2 className="text-5xl">TASTY TROVE</h2>
-        //                 <p className="text-3xl">Taste the treasure!</p>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
     );
 };
