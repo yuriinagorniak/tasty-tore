2f903239b8f2986d29ed266b3c5c579c6156dda6
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-06-21 13:37:10 +0100

    style(Inventory): add responsive styles to components

--- src/components/Inventory/AddProductInput.jsx
+++ src/components/Inventory/AddProductInput.jsx
@@ -8,6 +8,7 @@ import { useContext, useRef, useReducer } from "react";
 import { InventoryContext } from "../../contexts";
 import { useSnackbar, useFetchProduct } from "../../hooks";
 import { ProductSelect, MeasureSelect } from "./";
+import { TransparentButton } from "../../shared";
 
 const inputDebounce = (func, delay = 300) => {
     let timer;
@@ -18,7 +19,6 @@ const inputDebounce = (func, delay = 300) => {
         }, delay);
     };
 };
-
 
 export const AddProductInput = () => {
     const { addSingleProduct } = useContext(InventoryContext);
@@ -66,7 +66,7 @@ export const AddProductInput = () => {
                 image: selectedProduct.food.image ?? "",
                 quantity: quantityValue,
                 measure: selectedMeasure.label.toLowerCase(),
-                endDate: endDateValue
+                endDate: endDateValue,
             };
             addSingleProduct(newProduct);
             showMessage("product added", "success");
@@ -82,27 +82,33 @@ export const AddProductInput = () => {
     };
 
     return (
-        <section className="flex items-center justify-center gap-3 mb-10">
+        <section className="w-[350px] sm:w-[450px] md:w-[550px] lg:w-full m-auto flex flex-col lg:flex-row items-center justify-center gap-3 mb-10 sm:px-2">
             <p>Add a product:</p>
-            <ProductSelect
-                results={results}
-                handleChange={handleInputChange}
-                dispatch={dispatch}
-                state={state}
-                loading={loading}
-            />
-            <MeasureSelect state={state} dispatch={dispatch} />
+            <div className="w-full lg:w-[300px] lg:max-w-[300px]">
+                <ProductSelect
+                    results={results}
+                    handleChange={handleInputChange}
+                    dispatch={dispatch}
+                    state={state}
+                    loading={loading}
+                />
+            </div>
+            <div className="w-full lg:w-[300px] flex flex-row justify-between gap-3">
+                <div className="flex-1">
+                    <MeasureSelect state={state} dispatch={dispatch} />
+                </div>
+                <input
+                    className="w-full flex-1 pl-[14px] py-[16px] bg-[var(--additional-text-color)] rounded-sm"
+                    type="number"
+                    value={quantityInputValue}
+                    onChange={(e) => handleQuantityChange(e.target.value)}
+                    title="Quantity"
+                    min="0"
+                    step="0.1"
+                />
+            </div>
             <input
-                className="w-[130px] pl-[14px] py-[16px] bg-[var(--additional-text-color)] rounded-sm"
-                type="number"
-                value={quantityInputValue}
-                onChange={(e) => handleQuantityChange(e.target.value)}
-                title="Quantity"
-                min="0"
-                step="0.1"
-            />
-            <input
-                className="w-[160px] pl-[14px] py-[16px] bg-[var(--additional-text-color)] rounded-sm"
+                className="w-full lg:w-[300px] flex-1 pl-[14px] py-[16px] bg-[var(--additional-text-color)] rounded-sm"
                 type="date"
                 title="Expired by"
                 value={endDateValue ?? ""}
@@ -110,9 +116,9 @@ export const AddProductInput = () => {
                 min="0"
                 step="0.1"
             />
-            <button type="button" onClick={handleAddProduct}>
+            <TransparentButton className="w-full lg:max-w-[300px] lg:w-[100px]" handleClick={handleAddProduct}>
                 Add
-            </button>
+            </TransparentButton>
         </section>
     );
 };
--- src/components/Inventory/ListHeader.jsx
+++ src/components/Inventory/ListHeader.jsx
@@ -4,11 +4,11 @@ import { DownArrow } from "../../shared";
 export const ListHeader = ({ sortMethod = null, sortList = () => {} }) => {
     return (
         <>
-            <div className="w-[80%] m-auto flex items-center justify-end gap-2">
+            <div className="w-[95%] sm:w-[80%] m-auto flex items-center justify-end gap-2">
                 <p>Sort by</p>
                 {/* <SortButton sortMethod={sortMethod} handleChange={sortList} /> */}
             </div>
-            <div className="m-auto flex w-[75%] items-center justify-around text-xl font-bold">
+            <div className="m-auto flex w-[90%] sm:w-[75%] items-center justify-around text-[1rem] sm:text-xl font-bold">
                 <div className="flex-2 flex items-center cursor-pointer" onClick={sortList}>
                     <h3>Product</h3>
                     {sortMethod && <DownArrow rotated={sortMethod === "DESC"} />}
