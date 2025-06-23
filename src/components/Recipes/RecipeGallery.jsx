162bfaf0546daf98d9dd5d23e02724544d1044cc
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-06-23 18:39:02 +0100

    chore: update RecipeGallery

--- src/components/Recipes/RecipeGallery.jsx
+++ src/components/Recipes/RecipeGallery.jsx
@@ -3,13 +3,13 @@ import { useContext, useRef } from "react";
 import { RecipeFilter } from "./RecipeFilter";
 import { RecipeCard } from "../../components";
 import CircularProgress from "@mui/material/CircularProgress";
+import { TransparentButton } from "../../shared";
 
 import { RecipeContext } from "../../contexts/RecipeContextProvider/RecipeContext";
 
 
 export const RecipeGallery = () => {
     const { recipes, loading, error, handleNextPageSearch } = useContext(RecipeContext);
-
     return (
         <div className="w-[80%] md:w-[90%] lg:w-[70%] xl:w-[80%]">
             {error && <p>Something went wrong. Please try again.</p>}
@@ -18,12 +18,9 @@ export const RecipeGallery = () => {
                 <div className="flex flex-col gap-10 justify-center">
                     {recipes.length > 0 ? (
                         <>
-                            {/* <div className="w-[100%] flex flex-wrap items-center md:items-start justify-center lg:justify-start gap-10"> */}
                             <div className="m-auto w-full md:w-[90%] grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6 justify-items-center">
                                 {recipes.map((data) => (
                                     <div
-                                        // className="w-[325px] h-[165px] md:w-[280px] md:h-[145px] lg:w-[325px] lg:h-[165px]"
-                                        // className="w-[325px] h-[165px]"
                                         className="w-full aspect-[2/1]"
                                         key={data.recipe.uri}
                                     >
@@ -31,7 +28,7 @@ export const RecipeGallery = () => {
                                     </div>
                                 ))}
                             </div>
-                            <button onClick={() => handleNextPageSearch()}>Next page</button>
+                            <TransparentButton className="w-40 m-auto" handleClick={() => handleNextPageSearch()}>Next page</TransparentButton>
                         </>
                     ) : (
                         <p>Recipes not found</p>
